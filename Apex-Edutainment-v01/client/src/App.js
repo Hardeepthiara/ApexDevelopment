211ae2e7f0ef60917c4bdf558d9ed52aca134da4
Hardeep <hardeepkaur.bca@gmail.com>
2024-02-15 00:46:19 -0500

    Apex Basic Structure of MERN

--- Apex-Edutainment-v01/client/src/App.js
+++ Apex-Edutainment-v01/client/src/App.js
@@ -1,25 +1,29 @@
-// App.js
+
 import React from 'react';
 import {
    BrowserRouter as Router,
    Routes,
    Route
  } from "react-router-dom";
+import Register from './Screens/Register';
+import Login from './Screens/Login';
 import Form from './Form';
 import ContactForm from './ContactForm';
+import 'sweetalert2/dist/sweetalert2.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
    return (
       <div className="App">
           <Router>
-         <div>
-         <Routes>
-          <Route path="/register" element={<Form />} />
-         
-          <Route path="/contact-us" element={<ContactForm />} />
-
-        </Routes>
-         </div> 
+            <div>
+               <Routes>
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/form" element={<Form />} /> 
+                  <Route path="/contact-us" element={<ContactForm />} />
+               </Routes>
+            </div> 
        </Router>   
       </div>
    );
